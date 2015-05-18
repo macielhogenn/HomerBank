b78c09addbb1ef17907a7830f10d833513c7b6e5
Maciel Hogenn <maciel.hog@gmail.com>
2015-05-18 20:03:52 -0300

    Removed unused libraries

---
+++ src/java/br/com/homerbank/model/Agency.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.homerbank.model;
+
+/**
+ *
+ * @author Maciel
+ */
+public class Agency {
+    
+    private int id;
+    private String number;
+    
+}
